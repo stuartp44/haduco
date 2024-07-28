56427813bf51d610d9e8ec46471d4c9c98c75d41
Stuart Pearson <stuart.pearson@philips.com>
2024-07-28 18:49:45 +0200

    change of discovery

--- custom_components/duco/config_flow.py
+++ custom_components/duco/config_flow.py
@@ -1,13 +1,9 @@
 """Config flow for Duco integration."""
-
 from homeassistant import config_entries
 from homeassistant.components import zeroconf
 from homeassistant.core import callback
+from homeassistant.data_entry_flow import FlowResult
 from .const import DOMAIN
-import logging
-
-_LOGGER = logging.getLogger(__name__)
-
 
 class DucoConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Handle a config flow for Duco."""
@@ -18,19 +14,20 @@ class DucoConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         """Get the options flow for this handler."""
         return DucoOptionsFlowHandler(config_entry)
 
-    async def async_step_user(self, user_input=None):
+    async def async_step_user(self, user_input=None) -> FlowResult:
         """Handle the initial step."""
         return self.async_show_form(step_id="user")
 
-    async def async_step_zeroconf(self, discovery_info):
+    async def async_step_zeroconf(self, discovery_info: zeroconf.ZeroconfServiceInfo) -> FlowResult:
         """Handle a flow initiated by zeroconf discovery."""
-        # Extract details from discovery_info
-        _LOGGER.debug("Zeroconf discovery: %s", discovery_info)
-        name = discovery_info.get("name")
-        address = discovery_info.get("host")
-        port = discovery_info.get("port")
-        unique_id = discovery_info.get("properties", {}).get("id", name)
+        # Access the attributes directly
+        name = discovery_info.name
+        host = discovery_info.host
+        port = discovery_info.port
+        properties = discovery_info.properties
+        unique_id = properties.get("id", name)
 
+        # Set the unique ID and check if already configured
         await self.async_set_unique_id(unique_id)
         self._abort_if_unique_id_configured()
 
@@ -38,21 +35,19 @@ class DucoConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         return self.async_create_entry(
             title=f"Duco Device {name}",
             data={
-                "host": address,
+                "host": host,
                 "port": port,
                 "name": name,
                 "unique_id": unique_id,
             },
         )
 
-
 class DucoOptionsFlowHandler(config_entries.OptionsFlow):
     """Handle an options flow for Duco."""
-
     def __init__(self, config_entry):
         """Initialize options flow."""
         self.config_entry = config_entry
 
-    async def async_step_init(self, user_input=None):
+    async def async_step_init(self, user_input=None) -> FlowResult:
         """Manage the options."""
         return self.async_show_form(step_id="init")
