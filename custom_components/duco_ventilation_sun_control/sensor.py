d88ecd800a27f94d1bd9fdb903c876f7d871099a
Stuart Pearson <1926002+stuartp44@users.noreply.github.com>
2025-01-23 20:05:37 +0100

    Update sensor device classes and categories for Wi-Fi Signal Strength and Device Uptime

--- custom_components/duco_ventilation_sun_control/sensor.py
+++ custom_components/duco_ventilation_sun_control/sensor.py
@@ -17,6 +17,7 @@ from homeassistant.const import (
     UnitOfTime,
     PERCENTAGE,
     CONCENTRATION_PARTS_PER_MILLION,
+    EntityCategory,
 )
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.device_registry import DeviceInfo
@@ -52,7 +53,8 @@ COMMBOARD_SENSORS: tuple[DucoboxSensorEntityDescription, ...] = (
         name="Wi-Fi Signal Strength",
         native_unit_of_measurement="dBm",
         state_class=SensorStateClass.MEASUREMENT,
-        device_class=SensorDeviceClass.DIAGNOSTIC,
+        device_class=SensorDeviceClass.SIGNAL_STRENGTH,
+        entity_category=EntityCategory.DIAGNOSTIC,
         value_fn=lambda data: _process_rssi(
             data.get('General', {}).get('Lan', {}).get('RssiWifi', {}).get('Val')
         ),
@@ -63,7 +65,8 @@ COMMBOARD_SENSORS: tuple[DucoboxSensorEntityDescription, ...] = (
         name="Device Uptime",
         native_unit_of_measurement=UnitOfTime.SECONDS,
         state_class=SensorStateClass.TOTAL_INCREASING,
-        device_class=SensorDeviceClass.DIAGNOSTIC,
+        device_class=SensorDeviceClass.DURATION,
+        entity_category=EntityCategory.DIAGNOSTIC,
         value_fn=lambda data: _process_uptime(
             data.get('General', {}).get('Board', {}).get('UpTime', {}).get('Val')
         ),
